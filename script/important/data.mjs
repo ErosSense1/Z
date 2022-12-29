325d52a23bbb0606d1dc4a46e767abfcab33a624
ErosSense1 <tiktoksx@gmail.com>
2022-12-28 18:40:47 -0600

    0.0.0

--- script/important/data.mjs
+++ script/important/data.mjs
@@ -1,4 +1,4 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-const stage = document.querySelector('b')
+const stage = document.getElementById("stage");
 export { canvas, ctx, stage };
--- script/main.mjs
+++ script/main.mjs
@@ -23,8 +23,20 @@ function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   // draw bullets
-  player.weapon.bullets.forEach((bullet) => {
+  player.weapon.bullets.forEach((bullet, i) => {
     bullet.updateBullet();
+    if (bullet.pos.x < 0) {
+      player.weapon.bullets.splice(i, 1);
+    }
+    if (bullet.pos.y < 0) {
+      player.weapon.bullets.splice(i, 1);
+    }
+    if (bullet.pos.x > canvas.width) {
+      player.weapon.bullets.splice(i, 1);
+    }
+    if (bullet.pos.y > canvas.height) {
+      player.weapon.bullets.splice(i, 1);
+    }
   });
 
   // draws player
@@ -56,7 +68,7 @@ function gameLoop() {
     cancelAnimationFrame(ID);
   }
 }
-let zombies_Name = ['common','fast','tank','super']
+let zombies_Name = ["common", "fast", "tank", "super"];
 let stageNumber = 1;
 function stageLoop() {
   if (zombies.length === 1) {
@@ -66,23 +78,25 @@ function stageLoop() {
       let randomX = Math.floor(Math.random() * 2);
       let randomY = Math.floor(Math.random() * 2);
       let x = Math.floor(Math.random() * zombies_Name.length);
-      switch (zombies_Name[x]){
-        case 'common':
-          zombies.push(
-            new CommonZombie(positions[0][randomX], positions[1][randomY])
-          );
-          break;
-        case 'fast':
-          zombies.push(
-            new fastZombie(positions[0][randomX], positions[1][randomY])
-          );
-          break;
-        case 'tank':
-          zombies.push(new tankZombie(positions[0][randomX], positions[1][randomY]))
-          break;
-        case 'super':
-          zombies.push(new superZombie(positions[0][randomX], positions[1][randomY]))
-          break;
+      if (zombies_Name[x] == "common") {
+        zombies.push(
+          new CommonZombie(positions[0][randomX], positions[1][randomY])
+        );
+      }
+      if (zombies_Name[x] == "fast" && stageNumber > 3) {
+        zombies.push(
+          new fastZombie(positions[0][randomX], positions[1][randomY])
+        );
+      }
+      if (zombies_Name[x] == "tank" && stageNumber > 5) {
+        zombies.push(
+          new tankZombie(positions[0][randomX], positions[1][randomY])
+        );
+      }
+      if (zombies_Name[x] == "super" && stageNumber > 10) {
+        zombies.push(
+          new superZombie(positions[0][randomX], positions[1][randomY])
+        );
       }
     }
   }
